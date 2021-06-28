2f5f782fa30bde78fe3198af45896d5adffa943d
Abhijeet <abhijeetsathua2@gmail.com>
2021-06-28 23:42:44 +0530

    cors policy was updated

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const Port = process.env.PORT || 5001;
 // Middlewere
 app.use(
     cors({
-        origin: 'https://blissful-albattani-42cdb9.netlify.app',
+        origin: 'http://localhost:3000',
         credentials: true,
     })
 );
@@ -25,12 +25,12 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-    res.header("Content-Type", "application/json;charset=UTF-8");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Content-Type", "application/json;charset=UTF-8");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.get("/health", (req, res) => {
     res.send("Health ok...");
